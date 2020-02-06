285875dad385f706d605dcf39c6a318800af0af2
Amanda Atkins <amanda@digitalrainstorm.com>
2020-02-05 19:07:32 -0800

    Add GameActive key to GameContainer state to track current status of game

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -10,6 +10,7 @@ class GameContainer extends React.Component {
         score: 0,
         highScore: 0,
         albums: [],
+        gameActive: true,
         currentMessage: "Click a record to start the party!"
     }
 
@@ -21,36 +22,39 @@ class GameContainer extends React.Component {
         // Pick 16 random albums from album.js to setup our game
         // For now, we'll just get 16 album covers
         var randAlbums = [];
-        console.log(allAlbums);
         for (var i = 0; i < 16; i++) {
             randAlbums.push({
                 id: i,
+                artist: allAlbums[i].artist,
                 cover: allAlbums[i].albums[0]
             });
         }
         this.setState({ albums: randAlbums });
     }
 
-    randomizeAlbums = () => {
-        // Scramble the albums
-        return shuffle(this.albums);
-    }
+    // randomizeAlbums = (albums) => {
+    //     // Scramble the albums
+    //     return shuffle(albums);
+    // }
 
     endGame = () => {
         var reset = {
             score: 0,
+            gameActive: false,
             currentMessage: "You already played that. Buzz kill."
         };
         if (this.score > this.highScore) {
-            reset.highScore = this.score;
+            reset.highScore = this.state.score;
         }
         this.setState(reset);
     }
 
-    setScore = (id, isClicked) => {
+    setScore = (id, isClicked, message) => {
+        console.log("setting score");
         if (!isClicked) {
-            var updateAlbums = this.albums[id].isClicked = true;
-            this.setState({ score: this.score+1, currentMessage: "Righteous! Good choice.", albums: this.randomizeAlbums(updateAlbums)});
+            var updateAlbums = this.state.albums;
+            updateAlbums[id].isClicked = true;
+            this.setState({ score: this.state.score+1, currentMessage: message, albums: shuffle(updateAlbums), gameActive: true});
         } else {
             this.endGame();
         }
@@ -59,23 +63,28 @@ class GameContainer extends React.Component {
     render() {
             return (
                 <div>
-                    <ScoreBar score={this.score} highScore={this.highScore} currentMessage={this.currentMessage}/>
                     <div className="jumbotron">
-                        <h1 className="display-4">Party Music Pandemoneum!</h1>
-                        <p className="lead">You're hosting a party and spinning your favorite records! Click on an album to play it, but be careful to only play it once during the party lest you kill the mood!</p>
+                        <div className="container">
+                            <h1 className="display-4">Party Music Pandemoneum!</h1>
+                            <p className="lead">You're hosting a party and spinning your favorite records! Click on an album to play it, but be careful to only play it once during the party lest you kill the mood!</p>
+                            <br/>
+                            <p><strong>Highest Music Rating: </strong>{this.state.highScore}</p>
+                        </div>
                     </div>
-                    <div className="container">
+                    <ScoreBar score={this.state.score} currentMessage={this.state.currentMessage} gameActive={this.state.gameActive}/>
+                    <div className="container game">
                         <div className="row">
-                            {this.albums ? this.albums.map(album => (
-                                <div className="col-4 mb-3">
+                            {this.state.albums.map(album => (
+                                <div className="col-3 mb-4" key={album.id}>
                                     <Album 
-                                        cover={album.cover} 
+                                        cover={album.cover}
+                                        artist={album.artist}
                                         id={album.id}
-                                        key={album.id} 
                                         isClicked={album.isClicked}
+                                        setScore={this.setScore}
                                     />
                                 </div>
-                            )) : ""}
+                            ))}
                         </div>
                     </div>
 
