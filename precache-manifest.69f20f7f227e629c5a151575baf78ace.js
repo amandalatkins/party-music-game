ae8fcf82b25a8ff89a0287079f61a95f39dca5c1
Amanda Atkins <amanda@digitalrainstorm.com>
2020-02-06 11:40:27 -0800

    Updates

---
+++ precache-manifest.69f20f7f227e629c5a151575baf78ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7c5a37d44a5f2a4d269c30fdf0beca",
+    "url": "/party-music-game/index.html"
+  },
+  {
+    "revision": "824d03185a22be7daef3",
+    "url": "/party-music-game/static/js/2.080d8e07.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/party-music-game/static/js/2.080d8e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4258541c872484c51e9b",
+    "url": "/party-music-game/static/js/main.335d58a7.chunk.js"
+  },
+  {
+    "revision": "984cb498883e554ebbc6",
+    "url": "/party-music-game/static/js/runtime-main.2b9ddbea.js"
+  }
+]);
